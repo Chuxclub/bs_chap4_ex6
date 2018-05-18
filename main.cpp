c829042feef99308ced116dad18e6cedc44d56da
Florian <florian-1992@hotmail.fr>
2018-05-18 16:01:43 +0200

    Added prompt and began input loop

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 #include "std_lib_facilities.h"
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    cout << "Welcome to digit to words converter v0.1!" << '\n\n';
+    cout << "Please input any number from 0 to 9 written with digits or letters: " << '\n\n';
+
+    vector<string> numbers_dictionary;
+    numbers_dictionary = {"zero","one","two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    int user_input = 0;
+
+    while(cin >> user_input)
+    {
+
+    }
+
     return 0;
 }
