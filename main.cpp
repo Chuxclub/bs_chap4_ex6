4da032002e09f7fcb95e3904245fb74d6efae320
Florian <florian-1992@hotmail.fr>
2018-05-25 01:54:40 +0200

    Converter v0.1 release

--- main.cpp
+++ main.cpp
@@ -2,94 +2,147 @@
 #include "std_lib_facilities.h"
 
 void convert_digit(int a);
+void help();
 int convert_spelling(string a);
+int convert_string(string a);
 
-/* ~~~~~~~~~ Digits to spelling & spelling to digits converter ~~~~~~~~~ */
 
-int main() //If the user inputs a digit, main() converts it into its spelling and vice-versa
+/* ############ DIGITS TO SPELLING & SPELLING TO DIGITS CONVERTER ############ */
+
+//If the user inputs a digit, main() converts it into its spelling
+//Likewise, if the user inputs a spelled out number, main() converts it into its digit
+
+int main()
 {
-    int user_digit = 0;
-    string user_string, user_choice;
+    string user_digit, user_string, user_choice;
 
     cout << "\n\n" << "Welcome to digit to words converter!" << "\n\n";
-    cout << "Type 'help' for a list of available commands" << "\n\n";
+    cout << "Type 'help' for a list of available commands";
 
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Input loop ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    while(true)
+    while(1)
     {
+        cout << "\n\n";
         cout << "What's your selection? ";
         cin >> user_choice;
-        cout << "\n\n";
 
-        if (user_choice == "digit") //digits to spelling selection
+        //DIGITS to spelling selection
+        if (user_choice == "digit")
         {
-            cout << "Now input any number from 0 to 9 written with digits: ";
+            cout << '\n' << "Now input any number from 0 to 9 written with digits: ";
 
             while(cin >> user_digit)
             {
-                    cout << "\n\n";
-                    convert_digit(user_digit);
+                if(user_digit == "stop")
+                    break;
+
+                else if(user_digit == "help")
+                {
+                    help();
+                    cout << '\n' << "Now input any number from 0 to 9 written with digits: ";
+                }
+
+                else
+                    convert_digit(convert_string(user_digit));
             }
         }
 
-        else if (user_choice == "spelling") //spelling to digits selection
+        //SPELLING to digits selection
+        else if (user_choice == "spelling")
         {
-            cout << "Now input any spelled out number from zero to nine: ";
+            cout << '\n' << "Now input any spelled out number from zero to nine: ";
 
             while(cin >> user_string)
             {
-                cout << "\n\n";
-                convert_spelling(user_string);
-
                 if(user_string == "stop")
                     break;
+
+                else if(user_string == "help")
+                {
+                    help();
+                    cout << '\n' << "Now input any spelled out number from zero to nine: ";
+                }
+
+                else
+                    convert_spelling(user_string);
             }
 
         }
 
-        else if (user_choice == "quit") //program exit
+        //List of input commands always accessible to user
+        else if (user_choice == "help")
+            help();
+
+        //Program exit
+        else if (user_choice == "quit")
             break;
 
-        else if (user_choice == "help") //List of input commands always accessible
-        {
-            cout << "Type 'digit' to convert from spelled out numbers to digits..." << '\n';
-            cout << "Type 'spelling' to convert from digits to spelled out numbers. " << '\n';
-            cout << "Type 'stop' when in 'digit' or 'spelling' to return to main selection." << '\n';
-            cout << "Type 'quit' to end this program." << '\n';
-            cout << "Only numbers from 0 to 9 are currently accepted. " << "\n\n";
-        }
-
+        //Error handling
         else
-            cout << "Wrong input" << "\n\n"; //Wrong input value case
+            cout << endl << "Wrong input" << endl; //Wrong input value case
     }
 
     return 0;
 }
 
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions used ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/* ######################## Functions used ########################  */
 void convert_digit(int a)
 {
     vector<string> numbers_dictionary;
-    numbers_dictionary = {"zero","one","two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    numbers_dictionary = {"zero","one","two", "three", "four", "five",
+                          "six", "seven", "eight", "nine"};
 
     if(a >= 0 && a <= 9)
-        cout << numbers_dictionary[a] << "\n\n";
+        cout << " => " << numbers_dictionary[a] << "\n\n";
 
     else
-        cout << "Wrong input";
+        cout << "Wrong input, please enter any digit from 0 to 9: ";
 }
 
 int convert_spelling(string a)
 {
     vector<string> numbers_dictionary;
-    numbers_dictionary = {"zero","one","two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    numbers_dictionary = {"zero","one","two", "three", "four", "five",
+                          "six", "seven", "eight", "nine"};
+    bool wrong_input = false;
 
     for(int i = 0; i < 10; i++)
     {
         if(numbers_dictionary[i] == a)
         {
-            cout << i << "\n\n";
+            cout << "=> " << i << "\n\n";
             return i;
         }
+
+        else
+            wrong_input = true;
+
     }
+
+    if(wrong_input == 1)
+       cout << "Wrong input, please check spelling and try again: ";
+}
+
+int convert_string(string a)
+{
+    vector<string> digits_dictionary;
+    digits_dictionary = {"0","1","2", "3", "4", "5",
+                         "6", "7", "8", "9"};
+
+    for(int i = 0; i < 10; i++)
+    {
+        if (digits_dictionary[i] == a)
+            return i;
+    }
+}
+
+void help()
+{
+    cout << "\n\n";
+    cout << "Type 'digit' to convert from spelled out numbers to digits..." << '\n';
+    cout << "Type 'spelling' to convert from digits to spelled out numbers. " << '\n';
+    cout << "Type 'stop' when in 'digit' or 'spelling' to return to main selection." << '\n';
+    cout << "Type 'quit' to end this program." << '\n';
+    cout << "Only numbers from 0 to 9 are currently accepted. " << "\n\n";
 }
